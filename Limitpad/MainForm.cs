76d82eb6d0c83c41aca0100818d446619fa7c701
Victor <publicdomainvic@gmail.com>
2021-04-17 18:40:43 -0400

    Add OnLimitRichTextBoxTextChanged() code

--- Limitpad/MainForm.cs
+++ Limitpad/MainForm.cs
@@ -32,13 +32,76 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the limit rich text box text changed.
+        /// Handles the limit rich text box text changed event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
         private void OnLimitRichTextBoxTextChanged(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Colorize limit
+            this.ColorizeLimit();
+
+            // Update labels
+            this.characterCountToolStripStatusLabel.Text = this.limitRichTextBox.Text.Length.ToString();
+            this.wordCountToolStripStatusLabel.Text = this.CountByRegex(this.limitRichTextBox.Text).ToString();
+        }
+
+        /// <summary>
+        /// Colorizes the limit.
+        /// </summary>
+        private void ColorizeLimit()
+        {
+            // TODO Suspend layout [Can be handled via WM_PAINT]
+            this.limitRichTextBox.SuspendLayout();
+
+            // Set caret position
+            var selectionStart = this.limitRichTextBox.SelectionStart;
+
+            // Text
+            this.formatRichTextBox.Clear();
+            this.formatRichTextBox.ForeColor = this.prevColorDialog.Color;
+            this.formatRichTextBox.AppendText(this.limitRichTextBox.Text);
+
+            // Set selection start
+            try
+            {
+                // Process characters
+                if (this.charactersRadioButton.Checked)
+                {
+                    // Change color
+                    this.formatRichTextBox.SelectionStart = (int)this.limitNumericUpDown.Value;
+                    this.formatRichTextBox.SelectionLength = this.formatRichTextBox.TextLength - (int)this.limitNumericUpDown.Value;
+                    this.formatRichTextBox.SelectionColor = this.postColorDialog.Color;
+                }
+                else // Process words
+                {
+                    // Get word position list
+                    var wordPositionList = this.GetWordPositionList(this.formatRichTextBox.Text);
+
+                    // Set word selection start
+                    var wordSelectionStart = wordPositionList[(int)this.limitNumericUpDown.Value - 1];
+
+                    // Change color
+                    this.formatRichTextBox.SelectionStart = wordSelectionStart;
+                    this.formatRichTextBox.SelectionLength = this.formatRichTextBox.TextLength - wordSelectionStart;
+                    this.formatRichTextBox.SelectionColor = this.postColorDialog.Color;
+                }
+            }
+
+            catch
+            {
+                // Let it fall through
+            }
+
+            // Set rtf
+            this.limitRichTextBox.Rtf = this.formatRichTextBox.Rtf;
+
+            // Set cursor
+            this.limitRichTextBox.SelectionStart = selectionStart;
+            this.limitRichTextBox.SelectionLength = 0;
+
+            // Resume layout
+            this.limitRichTextBox.ResumeLayout();
         }
 
         /// <summary>
@@ -56,7 +119,49 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the post button click.
+        /// Gets the word position list.
+        /// </summary>
+        /// <returns>The word position list.</returns>
+        /// <param name="inputString">Input string.</param>
+        public List<int> GetWordPositionList(string inputString)
+        {
+            // The position list 
+            var positionList = new List<int>();
+
+            // The count
+            var count = 0;
+
+            // Iterate input string
+            for (var index = 1; index < inputString.Length; index++)
+            {
+                // Evaluate white space
+                if (char.IsWhiteSpace(inputString[index - 1]) == true)
+                {
+                    // Evaluate digit or punctuation
+                    if (char.IsLetterOrDigit(inputString[index]) == true || char.IsPunctuation(inputString[index]))
+                    {
+                        // Raise count
+                        count++;
+
+                        // Add word position
+                        positionList.Add(index - 1);
+                    }
+                }
+            }
+
+            // Raise by input string length
+            if (inputString.Length > 2)
+            {
+                count++;
+            }
+
+            // The populated position list
+            return positionList;
+        }
+
+
+        /// <summary>
+        /// Handles the post button click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -74,7 +179,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the previous button click.
+        /// Handles the previous button click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -92,7 +197,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the main tab control selected index changed.
+        /// Handles the main tab control selected index changed event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -102,7 +207,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the more releases public domain giftcom tool strip menu item click.
+        /// Handles the more releases public domain giftcom tool strip menu item click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -112,7 +217,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the original thread donation codercom tool strip menu item click.
+        /// Handles the original thread donation codercom tool strip menu item click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -122,7 +227,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the source code githubcom tool strip menu item click.
+        /// Handles the source code githubcom tool strip menu item click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -132,7 +237,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the about tool strip menu item click.
+        /// Handles the about tool strip menu item click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -142,7 +247,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the cut tool strip menu item click.
+        /// Handles the cut tool strip menu item click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -152,7 +257,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the copy tool strip menu item click.
+        /// Handles the copy tool strip menu item click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -162,7 +267,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the paste tool strip menu item click.
+        /// Handles the paste tool strip menu item click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -172,7 +277,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the select all tool strip menu item click.
+        /// Handles the select all tool strip menu item click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -182,7 +287,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the save tool strip menu item click.
+        /// Handles the save tool strip menu item click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -192,7 +297,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the save as tool strip menu item click.
+        /// Handles the save as tool strip menu item click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -202,7 +307,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the exit tool strip menu item click.
+        /// Handles the exit tool strip menu item click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -212,7 +317,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the new tool strip menu item click.
+        /// Handles the new tool strip menu item click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -222,7 +327,7 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the open tool strip menu item click.
+        /// Handles the open tool strip menu item click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
@@ -232,11 +337,21 @@ namespace Limitpad
         }
 
         /// <summary>
-        /// Ons the options tool strip menu item drop down item clicked.
+        /// Handles the options tool strip menu item drop down item clicked event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
         /// <param name="e">Event arguments.</param>
         private void OnOptionsToolStripMenuItemDropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            // Add code
+        }
+
+        /// <summary>
+        /// Handles the limit numeric up down value changed event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnLimitNumericUpDownValueChanged(object sender, EventArgs e)
         {
             // Add code
         }
