fd71b371a9e1eea8d19cdef3af5c793f6d1525cb
Victor <publicdomainvic@gmail.com>
2021-04-17 20:17:32 -0400

    Add SaveSettingsFile() function

--- Limitpad/MainForm.cs
+++ Limitpad/MainForm.cs
@@ -393,5 +393,31 @@ namespace Limitpad
                 return xmlSerializer.Deserialize(fileStream) as SettingsData;
             }
         }
+
+        /// <summary>
+        /// Saves the settings file.
+        /// </summary>
+        /// <param name="settingsFilePath">Settings file path.</param>
+        /// <param name="settingsDataParam">Settings data parameter.</param>
+        private void SaveSettingsFile(string settingsFilePath, SettingsData settingsDataParam)
+        {
+            try
+            {
+                // Use stream writer
+                using (StreamWriter streamWriter = new StreamWriter(settingsFilePath, false))
+                {
+                    // Set xml serialzer
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(SettingsData));
+
+                    // Serialize settings data
+                    xmlSerializer.Serialize(streamWriter, settingsDataParam);
+                }
+            }
+            catch (Exception exception)
+            {
+                // Advise user
+                MessageBox.Show($"Error saving settings file.{Environment.NewLine}{Environment.NewLine}Message:{Environment.NewLine}{exception.Message}", "File error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
