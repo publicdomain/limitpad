1e277c2449a924dc55b03e92701156e98980e7d8
Victor <publicdomainvic@gmail.com>
2021-04-17 21:25:30 -0400

    Add OnOptionsToolStripMenuItemDropDownItemClicked() code

--- Limitpad/MainForm.cs
+++ Limitpad/MainForm.cs
@@ -385,7 +385,11 @@ namespace Limitpad
         /// <param name="e">Event arguments.</param>
         private void OnOptionsToolStripMenuItemDropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
-            // Add code
+            // Set clicked item
+            var clickedItem = (ToolStripMenuItem)e.ClickedItem;
+
+            // Toggle checked
+            clickedItem.Checked = !clickedItem.Checked;
         }
 
         /// <summary>
